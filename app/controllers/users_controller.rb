c575e58f70daf3335ec16b21879cad92ab8960dc
Dat Luong <unlimited2989@gmail.com>
2022-08-24 17:58:35 +0700

    Add User Edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,27 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def edit
+
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+
+        respond_to do |format|
+            if @user.update(user_params)
+                flash[:notice] = "Your Account information was successfully updated"
+            redirect_to articles_path
+            else
+                format.html { render :new, status: :unprocessable_entity }
+                format.json { render json: @user.errors, status: :unprocessable_entity }
+            end
+        end
+
+
+    end
+
     def create
         @user = User.new(user_params)
         respond_to do |format|
@@ -16,6 +37,7 @@ class UsersController < ApplicationController
         end
     end
 
+    
 
     private
     def user_params
